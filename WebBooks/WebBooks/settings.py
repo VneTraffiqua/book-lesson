2411044bc8f03227ea6809ba13384c040897666c
VneTraffiqua <safe72again@gmail.com>
2022-05-26 08:37:02 -0700

    COMMIT2

--- WebBooks/WebBooks/settings.py
+++ WebBooks/WebBooks/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=#xsnb&%4di3djfen+iy!1i_q@^pffz7lie#ibf&w7gfg4*t+!'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
